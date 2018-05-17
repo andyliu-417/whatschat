a6ead01c6f41c38647e450ee37ba25f12fbdd709
Andy Junxi <andyjunxi@Andys-MacBook-Pro.local>
2018-05-17 10:44:30 -0400

    signup ui

--- web/client/src/containers/Signup/Signup.js
+++ web/client/src/containers/Signup/Signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Signup.css";
+import { Link } from "react-router-dom";
 
 class Signup extends Component {
   constructor(props) {
@@ -8,9 +9,65 @@ class Signup extends Component {
   }
 
   componentDidMount() {}
-  
+
   render() {
-    return <div>Signup</div>;
+    return (
+      <div className="container">
+        <div className="card-panel signup-panel">
+          <form className="col s12" action="/" >
+            <h4 className="center-align">Sign Up</h4>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  className="validate"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="validate"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="confirm_password"
+                  type="password"
+                  name="confirm_password"
+                  className="validate"
+                />
+                <label htmlFor="confirm_password">Confirm Password</label>
+              </div>
+            </div>
+            <div className="row right-align">
+              <input
+                type="submit"
+                className="waves-effect waves-light btn indigo lighten-1"
+                value="Sign Up"
+              />
+            </div>
+            <div className="row">
+              <p className="right-align">
+                {" "}
+                Already have an account? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
