a1f05cb0761d7e79b4ce103b328fe53b1e0d4b84
Andy Junxi <andyjunxi@Andys-MacBook-Pro.local>
2018-05-17 22:42:14 -0400

    fix bug show username

--- web/client/src/components/ChatHeader/ChatHeader.js
+++ web/client/src/components/ChatHeader/ChatHeader.js
@@ -3,25 +3,34 @@ import "./ChatHeader.css";
 import { Avatar } from 'antd';
 
 import { connect } from "react-redux";
-import {getFriendList} from '../../redux/friend.redux';
+import { getMsgList } from "../../redux/chat.redux";
+import { recvMsg } from "../../redux/chat.redux";
 
-// @connect(
-// 	state=>state.friend,
-// 	{getFriendList}
-// )
+
+@connect(state => state.chat, { getMsgList, recvMsg })
 class ChatHeader extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  componentDidMount() {
+    if (!this.props.chatmsg.length) {
+      this.props.getMsgList();
+      this.props.recvMsg();
+    }
+  }
   render() {
     const userid = this.props.match.params.user;
-    
+    const users = this.props.users;
+    if(!users[userid]) {
+      return null;
+    }
     return (
       <div className="contacttitle-component">
         <div className="content-wrap">
-        {this.props.match.params.user}
+        {/* {this.props.match.params.user} */}
+        {users[userid].username}
         {/* {this.props.friendList.filter(v=>v._id==userid).username} */}
         </div>
       </div>
