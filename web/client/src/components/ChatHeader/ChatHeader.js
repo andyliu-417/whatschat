622a33a74c76226fc4a51de8b770d2e256f3d6ba
Andy Junxi <andyjunxi@Andys-MacBook-Pro.local>
2018-05-18 11:08:08 -0400

    header avatar

--- web/client/src/components/ChatHeader/ChatHeader.js
+++ web/client/src/components/ChatHeader/ChatHeader.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import "./ChatHeader.css";
-import { Avatar } from 'antd';
+import { Avatar } from "antd";
 
 import { connect } from "react-redux";
 import { getMsgList } from "../../redux/chat.redux";
 import { recvMsg } from "../../redux/chat.redux";
-
 
 @connect(state => state.chat, { getMsgList, recvMsg })
 class ChatHeader extends Component {
@@ -23,14 +22,15 @@ class ChatHeader extends Component {
   render() {
     const userid = this.props.match.params.user;
     const users = this.props.users;
-    if(!users[userid]) {
+    if (!users[userid]) {
       return null;
     }
     return (
       <div className="contacttitle-component">
-        <div className="content-wrap">
-        {users[userid].username}
+        <div className="photo-wrap">
+          <Avatar shape="circle" src={require(`../avatars/${users[userid].avatar}.png`)} />
         </div>
+        <div className="content-wrap">{users[userid].username}</div>
       </div>
     );
   }
